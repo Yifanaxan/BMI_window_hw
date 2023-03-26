002d698aa3c3351d38e95a98cfecc4180b1aa6d4
Yifan_Lai <yifanlai8@gmail.com>
2023-03-26 10:29:14 +0800

    add color

---
+++ entrycolor.py
@@ -0,0 +1,13 @@
+
+#only show color whwn the entry is "PACK"
+
+from tkinter import *
+
+root = Tk()
+
+# Create a grid entry widget and pack it
+entry = Entry(root, bg="yellow")
+entry.pack()
+
+root.mainloop()
+
